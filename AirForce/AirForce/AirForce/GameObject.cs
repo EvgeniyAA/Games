518526f65755fd0fe6b7301d458fe9384a32c173
EvgeniyAA <andreeff.ea@yandex.ru>
2016-08-11 22:37:21 +0300

    renamed from object to GameObject

---
+++ AirForce/AirForce/AirForce/GameObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+
+namespace AirForce
+{
+    public abstract class GameObject
+    {
+        public int Speed;
+        public int Hp;
+        public Direction ObjectDirection;
+        public ObjectType ObjectType;
+        public Point GameObjectPoint;
+        public Point GameObjectSize;
+        public int DodgeCoord;
+
+        protected GameObject(Point gameObjectPoint)
+        {
+            GameObjectPoint = gameObjectPoint;
+        }
+
+        protected GameObject()
+        {
+        }
+
+        public virtual Direction GetDirection()
+        {
+            return ObjectDirection;
+        }
+
+        public virtual void Move()
+        {
+            GameObjectPoint.X -= Speed;
+        }
+
+        public void TakeDamage()
+        {
+            Hp--;
+        }
+
+        public abstract void Draw(Graphics graphics);
+    }
+}
