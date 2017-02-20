30a767dff7a0759203a88605b4cdf91e9b493909
EvgeniyAA <andreeff.ea@yandex.ru>
2017-02-20 20:54:05 +0200

    bugfix(stick didnt recalculate length)

--- StickHero/Segment.cs
+++ StickHero/Segment.cs
@@ -8,23 +8,33 @@ namespace StickHero
         public Point PointA;
         public Point PointB;
 
-        public double Length;
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(PointB.Y - PointA.Y, 2) +
+                               Math.Pow(PointB.X - PointA.X, 2));
+            }
+            set { length = value; }
+        }
+
         public int Angle;
 
+        private double length;
         //  рутим по часовой стрелке относительно точки PointA
         public void Rotate(int angle)
         {
             Angle -= angle;
 
-            PointB.X = (int)(Length * Math.Cos(Math.PI / 180 * Angle) + PointA.X);
-            PointB.Y = (int)(PointA.Y - Length * Math.Sin(Math.PI / 180 * Angle));
+            PointB.X = (int)(length * Math.Cos(Math.PI / 180 * Angle) + PointA.X);
+            PointB.Y = (int)(PointA.Y - length * Math.Sin(Math.PI / 180 * Angle));
         }
 
         // “очка PointA стоит на месте, а увеличиваем длину в сторону PointB
         public void Grow(int animationSpeed)
         {
             PointB.Y -= animationSpeed;
-            Length = Math.Sqrt(Math.Pow(PointB.Y - PointA.Y, 2) +
+            length = Math.Sqrt(Math.Pow(PointB.Y - PointA.Y, 2) +
                                Math.Pow(PointB.X - PointA.X, 2));
         }
     }
